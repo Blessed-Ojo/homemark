70ccabff1ffa0d9caafdeac820eab7264355b587
Blessed <ojoblessed533@gmail.com>
2024-08-12 15:36:13 -0400

    "Added react-lazy-load-image-component dependency and implemented LazyLoadImage in Experience2 component, updated package-lock.json and package.json accordingly, and made minor changes to tools.scss"

--- src/components/experience/Experience2.jsx
+++ src/components/experience/Experience2.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import "./experince.scss";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { xpdata } from "../../lib/xp";
 const Experience2 = () => {
+  
+  
   return (
-    <div id='experience'>Experience2</div>
-  )
-}
+    <div className="Experience2">
+      <div className="Experience-text">
+        {"<"} Here, you can know me a little more and see my
+        <br />
+        whole experience as a <span>Front-End Software Engineer</span>. {"/>"}
+      </div>
+      <div className="experince-wapper">
+        {xpdata.map((item, i) => (
+          <div
+            key={i}
+            className="experince-item"
+            
+          >
+            <div className="left">
+              <h1>{item.name}</h1>
+              <p>{item.xp} :</p>
+              <h4>{item.tittle}</h4>
+            </div>
+            <div className="right">
+              <p>{item.text}</p>
+              <LazyLoadImage
+                src={item.image}
+                alt=""
+                effect="blur" 
+              />
+              <button>
+                <a href={item.link}>Demo</a>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Experience2
+export default Experience2;
